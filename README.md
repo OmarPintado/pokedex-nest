4db1484167b8ae3c85f37b0dafb5f6448bc08f4c
GaOmPS <omarpintado98@gmail.com>
2024-01-10 11:17:21 -0500

    Feat: execute mongodb seed

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 2. Ejecutar ```yarn install```
 3. Tener Nest CLI instalado ```npm i -g@nestjs/cli```
 4. Levantar la base de datos ```docker compose up -d```
+5. Reconstruir la base de datos ```http://localhost:3000/api/v2/seed```
 
 ## Stack usado
 * MongoDB
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponseInterface } from './poke-response.interface';
+import { PokemonService } from '../pokemon/pokemon.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({}); // delete * from pokemon;
+
+    const { data } = await this.axios.get<PokeResponseInterface>(
+      'https://pokeapi.co/api/v2/pokemon?limit=100',
+    );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    for (const { name, url } of data.results) {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      pokemonToInsert.push({ no: no, name: name });
+    }
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return data.results;
+  }
+}
